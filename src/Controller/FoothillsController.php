93d2a3d7aca7a5842874119763d3c7d61e96e812
Tac Tacelosky <tacman@gmail.com>
2022-03-20 15:26:12 -0400

    add ff articles and qr codes

---
+++ src/Controller/FoothillsController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\Story;
+use App\Entity\UrlCache;
+use Goutte\Client;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class FoothillsController extends AbstractController
+{
+    public function __construct(private CacheInterface $cache, private HttpClientInterface $httpClient) {
+
+    }
+
+    #[Route('/foothills', name: 'app_foothills')]
+    public function index(): Response
+    {
+        return $this->render('foothills/index.html.twig', [
+            'articles' => $this->scrapeArticles(),
+            'controller_name' => 'FoothillsController',
+        ]);
+    }
+
+    private function scrapeArticles()
+    {
+
+        $url = 'https://www.rappnews.com/news/foothills/';
+        $html = $this->cache->get(md5($url), function(ItemInterface $item) use ($url) {
+            $item->expiresAfter(60 * 60 * 24);
+            // actually do the fetch
+            return $this->httpClient->request('GET', $url)->getContent();
+//        $response  = $this->httpClient('GET', $url);
+        });
+        $crawler = new Crawler($html, $url);
+
+        $articles = [];
+
+        $x = $crawler->filter('.card-body a.tnt-asset-link')->each(function(Crawler $node) use ($articles) {
+//            dd($node, $node->outerHtml(), $node->links()[0]);
+            $base = 'https://www.rappnews.com';
+            return new Story(headline: $node->innerText(), url: $base . $node->attr('href'));
+        });
+        return $x;
+        dd($x);
+        return $articles;
+
+        $crawler->filter('.card-body a.tnt-asset-link')->each(function(Crawler $node) {
+           dd($node->innerText(), $node->outerHtml());
+        });
+        return $html;
+
+    }
+
+}
---
+++ src/DTO/Story.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\DTO;
+
+class Story {
+    public function __construct(public string $headline, public string $url) {
+
+    }
+}
